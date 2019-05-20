5664f8131b2f5ae7eb9b563a28aca5cdeb3d5f36
Colin Lees <colin.lees@stockport.gov.uk>
2019-05-20 11:02:48 +0100

    fix: Rename file to match class name

---
+++ src/ServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace StockportGovUK.AspNetCore.Analytics.Google
+{
+    public  static class ServiceCollectionExtensions
+    {
+        public static void AddGoogleAnalytics(this IServiceCollection services)
+        {
+            services.AddSingleton<IEventTrackingHelper, EventTrackingHelper>();
+        }
+    }
+}
